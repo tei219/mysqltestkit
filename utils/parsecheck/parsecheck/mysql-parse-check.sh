77a0d0571e3b33bd2a1d9f12f5717ffffddb6394
tei219 <tei219@gmail.com>
2022-03-31 16:49:20 +0900

    add check instance

--- utils/parsecheck/parsecheck/mysql-parse-check.sh
+++ utils/parsecheck/parsecheck/mysql-parse-check.sh
@@ -13,13 +13,13 @@ echo "--------"
 
 for instance in mysql80 mysql57 mysql56
 do
-    ver=$(mysql -h $instance -sNe "select @@version;")
-    if [[  $instance == "mysql80" ]]; then
-        mysql -h $instance -sNe "select STATEMENT_DIGEST_TEXT('$stmt');" mysql 2>parseerror.log 1>/dev/null
-    else
-        mysql -h $instance -sNe "explain $stmt;" mysql 2>parseerror.log 1>/dev/null
-    fi
-    if [[ ! -z parseerror.log ]]; then
-    echo "MySQL $ver: $(cat parseerror.log)"
+    if nslookup $instance >/dev/null ; then
+        ver=$(mysql -h $instance -sNe "select @@version;")
+        if [[  $instance == "mysql80" ]]; then
+            mysql -h $instance -sNe "select STATEMENT_DIGEST_TEXT('$stmt');" mysql 2>parseerror.log 1>/dev/null
+        else
+            mysql -h $instance -sNe "explain $stmt;" mysql 2>parseerror.log 1>/dev/null
+        fi
+        echo "MySQL $ver: $(cat parseerror.log)"
     fi
 done
